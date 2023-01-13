bdda53bd6fc8305b5aa186fe80bfc9a20844ea5e
marlborocool <greatrj890106@gmail.com>
2023-01-13 12:31:38 +0900

    feat: Reset, Upvote, Downvote complete

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
 
-export default function Button() {
+export default function Button({ children, onClick, isLoading, isDisabled, type }) {
+  let backgroundColor = 'blue';
+  if (type === 'submit') {
+    backgroundColor = 'blue';
+  } else if (type === 'cancel') {
+    backgroundColor = 'red';
+  } else if (type === 'link') {
+    backgroundColor = 'green';
+  }
 
+  return (
+    <button style={{ backgroundColor: isDisabled ? 'lightblue' : backgroundColor, color: 'white', borderRadius: '5px', cursor: 'pointer' }} disabled={isDisabled} onClick={onClick}>
+      {isLoading ? <CircularProgress /> : children}
+    </button>
+  )
 } 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,56 +1,129 @@
 import React, { useEffect, useState } from 'react';
 import useDebounce from '../hooks/useDebounce';
-import { TextField, Typography, List, ListItem, ListItemText, ListItemIcon } from '@mui/material';
+import {
+  TextField, Typography,
+  Table, TableBody, TableCell, TableHead, TableRow, Paper,
+  Card, CardHeader, CardMedia, CardContent
+} from '@mui/material';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbDownAltIcon from '@mui/icons-material/ThumbDownAlt';
 import FadeIn from 'react-fade-in';
 import axios from 'axios';
+import Button from '../components/Button';
 
-let initialVotes = [];
-for (let i = 0; i < 30; i++) {
-  initialVotes.push(Math.floor(Math.random() * 20));
-}
+let initialData = [];
 
 export default function Home() {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
+  const [sortedData, setSortedData] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+  const debouncedSearchTerm = useDebounce(searchTerm, 200);
+
+  const getData = async () => {
+    const result = (await axios.get(process.env.REACT_APP_API_ENDPOINT + `&q=${debouncedSearchTerm}`)).data.data;
+    initialData = [...result];
+    console.log('data', initialData);
+    setData(result)
+  }
 
   useEffect(() => {
-    const getData = async () => {
-      const result = (await axios.get(process.env.REACT_APP_API_ENDPOINT)).data.data;
-      setData(result.map((item, index) => {
-        item.vote = initialVotes[index]
-        return item;
-      }));
-    }
     getData();
   }, [debouncedSearchTerm]);
+
+  useEffect(() => {
+    let temp = [...data];
+    setSortedData(temp.sort((a, b) => { return b.favorites - a.favorites }));
+  }, [data]);
+
+  const updateVote = (title, increment) => {
+    const updated = data.map(item => {
+      if (item.title === title) {
+        if (increment !== -1 || item.favorites !== 0)
+          item.favorites += increment;
+      };
+      return item;
+    })
+    setData(updated);
+    console.log(initialData);
+  }
 
   return (
     <>
       <Typography variant='h3' gutterBottom>
-        Anime List
+        Search Anime
       </Typography>
-      <TextField label={'Search By Name'} margin="dense" value={searchTerm} onChange={(e) => { setSearchTerm(e.target.value) }} gutterBottom />
-      <List>
-        <FadeIn>
-          {data.map((item, index) => (
-            <ListItem key={index}>
-              <img src={item.images.webp.image_url} alt='alt' />
-              <ListItemText>
-                <Typography variant='h5'>
+      <TextField label={'Search By Name'} margin="dense" value={searchTerm} onChange={(e) => { setSearchTerm(e.target.value) }} />
+      <br />
+      <Button onClick={() => { getData(); }}>Reset Votes</Button>
+      <FadeIn>
+        <Paper style={{ display: 'flex', maxWidth: '500px', justifyContent: 'space-between', margin: 'auto', marginTop: '20px', padding: '10px' }}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              image={sortedData.length ? sortedData[0].images.webp.image_url : ''}
+              height="225"
+              alt="Paella dish"
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {`Most Upvoted: ${sortedData.length ? sortedData[0].favorites : ''}`}
+              </Typography>
+            </CardContent>
+            <CardHeader title={sortedData.length ? sortedData[0].title : ''} />
+          </Card>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              image={sortedData.length ? sortedData[sortedData.length - 1].images.webp.image_url : ''}
+              height="225"
+              alt="Paella dish"
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {`Most Downvoted: ${sortedData.length ? sortedData[sortedData.length - 1].favorites : ''}`}
+              </Typography>
+            </CardContent>
+            <CardHeader title={sortedData.length ? sortedData[sortedData.length - 1].title : ''} />
+          </Card>
+        </Paper>
+      </FadeIn>
+      <Paper className='table-container'>
+        <table>
+          <caption>Anime List</caption>
+          <thead>
+            <tr>
+              <th scope="col">Anime</th>
+              <th scope="col">Type</th>
+              <th scope="col">Votes</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>
                   {item.title}
-                </Typography>
-              </ListItemText>
-              <ListItemText>
-                {`${item.vote} Votes`}
-              </ListItemText>
-              <ListItemIcon>
-
-              </ListItemIcon>
-            </ListItem>
-          ))}
-        </FadeIn>
-      </List>
+                </td>
+                <td>
+                  {item.type}
+                </td>
+                <td>
+                  {`${item.favorites} Votes`}
+                </td>
+                <td>
+                  <Button type="submit" onClick={() => updateVote(item.title, 1)}>
+                    <ThumbUpIcon />
+                  </Button>
+                  {' '}
+                  <Button type="cancel" onClick={() => updateVote(item.title, -1)}>
+                    <ThumbDownAltIcon />
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Paper>
     </>
   )
 }
